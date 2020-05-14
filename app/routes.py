d0f72766681e39158cdb05543a329798c44ead47
Houda Belefqih <houdabelefqih@gmail.com>
2020-05-14 00:55:30 +0000

    added display of Pascal's triangle with buttons.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,18 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    from operator import add
+
+    numRows = 17
+    res = [[1]]
+    for _ in range(1, numRows):
+        map_ = map(add, [0] + res[-1], res[-1] + [0])
+        res.append(list(map_))
+
+    pascal=  res if numRows else []
+
+    return render_template('index.html', title='Pascal\'s Briwates', pascal=pascal)
