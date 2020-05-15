83050778e587fdf0b846da043e4a6f755e1caeeb
Houda Belefqih <houdabelefqih@gmail.com>
2020-05-15 02:10:04 +0000

    added .gitattributes file

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, IntegerField
+from wtforms import IntegerField
 from wtforms.validators import InputRequired, NumberRange
 
 
 class PascalForm(FlaskForm):
-    numRow = IntegerField('', validators=[InputRequired(), NumberRange(min=1, max=23, message= "Row number should be in the range [1-23]")])
-    submit = SubmitField('GO')
+    numRow = IntegerField('', validators=[InputRequired(), NumberRange(min=1, max=23, message="Row number should be in the range [1-23]")])
